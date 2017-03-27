5f4e5727f50ff6f8a70c047536781e177733fe52
Patrick Elliott <p.cd.elliott@gmail.com>
2017-03-27 20:05:47 +1100

    A test file to compute the E_6

---
+++ test.py
@@ -0,0 +1,14 @@
+import configs_draft
+import time
+import itertools
+import new_attempt
+
+
+
+pot = ((3,0),(0,2))   # representation of the decomposed potential W = x^3 + y^2
+
+
+for t in range(3,13):
+    print("---------------")
+    for tree in configs_draft.BinTreeGen(t):
+        print(configs_draft.EnumerateConfigs(tree, pot))
