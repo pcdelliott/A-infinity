333b09c75b347a49e6c48c3f47b2320f63d16b91
Patrick Elliott <p.cd.elliott@gmail.com>
2017-04-03 17:30:42 +1000

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,8 @@ import new_attempt
 pot = ((3,0),(0,2))   # representation of the decomposed potential W = x^3 + y^2
 
 
-for t in range(3,15):
-    print("---------------")
+for t in range(3,13):
+    print('Trees with {} leaves'.format(t))
     for tree in configs_draft.BinTreeGen(t):
         print(configs_draft.EnumerateConfigs(tree, pot))
+
